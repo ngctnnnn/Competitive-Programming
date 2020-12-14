7442ee7eef387be10d362300ec22ed8ff0ffd65f
user <19520925@gm.uit.edu.vn>
2020-12-14 16:34:30 +0700

    add template

---
+++ template.h
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define loop(i, a, b, c) for(int i= a; i< b; i+= c)
+#define loopback(i,a,b,c) for(int i=(a),_b=(b); i>=_b; i-=c)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define each(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; loop(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+int main() {
+
+
