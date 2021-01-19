c02f37d7ab9ae643735d242fada52bc7276569f3
ngctnnnn <19520925@gm.uit.edu.vn>
2021-01-19 12:53:03 +0700

    Jam 19

---
+++ CSES/COUNTING_ROOMS.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define loop(i, a, b, c) for(__typeof(a) i=(a), _b=(b), _c=(c); i<_b; i+=_c)
+#define loopeach(i, a, b) for(__typeof(a) i = (a), _b=(b); i <_b;++i)
+#define loopback(i,a,b,c) for(__typeof(a) i=(a),_b=(b), _c=(c); i>=_b; i-=_c)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define ll long long
+#define read(n) int n; cin >> n
+#define readln(a,n) int n; cin >> n; int a[n]; loopeach(i, 0, n) cin >> a[i];
+#define vii vector<pair<int, int>> 
+#define vll vector<pair<long long, long long>> 
+#define each(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) {loop(_, 0,n, 1) cout << a[_] << ' '; cout << endl; }
+#define sqr(x) ((x) * (x)) 
+#define ff first
+#define ss second 
+#define oo 1e99
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    freopen("DEBUG.INP", "r", stdin);
+    freopen("DEBUG.OUT", "w", stdout);
+
+    read(n);
+    read(m);
+    char c[n + 1][m + 1];
+    int cnt = 0;    
+
+    loopeach(i, 1, n + 1)
+        loopeach(j, 1, m + 1)
+            cin >> c[i][j];
+
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j){ 
+            if (c[i][j] =='.'){
+                cnt++;
+                break;
+            }
+    }
+    cout << cnt << endl;
+    return 0;
+}
