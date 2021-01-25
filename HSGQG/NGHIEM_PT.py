e295d3c4432eac3c31f0fa8e51de54b2498c7761
ngctnnnn <19520925@gm.uit.edu.vn>
2021-01-25 22:54:10 +0700

    Jan 25

---
+++ HSGQG/NGHIEM_PT.py
@@ -0,0 +1,16 @@
+def giaithua(dp, n):
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = i*dp[i - 1]
+
+def chinhhop(dp, k, n):
+    return dp[n] / (dp[k] *dp[n - k])
+
+k = int(input())
+n = int(input())        
+dp = [1 for _ in range(n + k + 2)]
+
+giaithua(dp, n + k + 1)
+
+print(chinhhop(dp, k - 1, n + k - 1))
