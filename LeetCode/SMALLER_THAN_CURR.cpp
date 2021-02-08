5d25478f0dd5ba2584fbc2356685ba95a977a155
ngctnnnn <19520925@gm.uit.edu.vn>
2021-02-08 22:39:55 +0700

    Feb 8

---
+++ LeetCode/SMALLER_THAN_CURR.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); ++i){ 
+            int cnt = 0;
+            for (int j = 0; j < nums.size(); ++j) {
+                if (nums[j] < nums[i])
+                    cnt++;
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
